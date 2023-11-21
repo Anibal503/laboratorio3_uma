f0ad7bd3e1cc7b5118a745a68358a23e14c6a4c8
Anibal <moto333vas@gmail.com>
2023-11-20 20:12:10 -0600

    Creando archivo y calse de carro

---
+++ carro.py
@@ -0,0 +1,16 @@
+class Carro:
+    pass
+ruedas = 4
+puertas = 0 
+ventanas = 0
+color = ""
+marca = 0
+
+sedan = Carro()
+sedan.marca = "nissan"
+sedan.color = "Azul"
+print(sedan.color)
+
+microbus = Carro()
+
+
