63e1095972bcc5cddf2a26eb4d5ed4e2a9ae9e67
Anibal <moto333vas@gmail.com>
2023-11-22 19:40:07 -0600

    Creando la clase Auto y sus especificaiones

---
+++ Laboratorio3.py
@@ -0,0 +1,12 @@
+class Auto:
+    def __init__(auto, marca, modelo, asientos, color, tipo, combustible, precio, garantia, fecha_ingreso):
+        auto.marca = marca
+        auto.modelo = modelo
+        auto.asientos = asientos
+        auto.color = color
+        auto.tipo = tipo
+        auto.combustible = combustible
+        auto.precio = precio
+        auto.garantia = garantia
+        auto.fecha_ingreso = fecha_ingreso
+
